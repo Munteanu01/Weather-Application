3067960f9d976d46e9a71d381b251f716e179d8a
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-04-05 21:35:48 +0300

    optimizing code

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 import { key } from './config.js';
 
+const search = document.querySelector('#search')
+const locationButton = document.querySelector('#locationButton') 
+let api;
+
 navigator.geolocation.getCurrentPosition(ifSucces, ifError)
 function ifSucces(position){
    const {latitude, longitude} = position.coords;
-   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
-   fetch(api)
-    .then(response => response.json())
-    .then(data => {
+   api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
+   fetchData()
+}
+function ifError(){
+    alert(`Something didn't work well :(\nCheck if you allowed the website to acces your location`);
+}
+locationButton.addEventListener('click', ()=> {
+    navigator.geolocation.getCurrentPosition(ifSucces, ifError)
+})
+
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && search.value != ""){
+        requestApi(search.value)
+    }
+})
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+    fetchData();
+}
+function fetchData(){
+    fetch(api).then(response => response.json()).then(data => weatherData(data))
+}
+function weatherData(data){
+    if(data.cod == "404"){
+        document.querySelector('#infoText').innerText = `${search.value} isn't a valid city name`;
+        document.querySelector('#weatherDiv').style.display = 'none';
+    }else{
         document.querySelector('#locationName').innerText = `You are currently in ${data.name}, ${data.sys.country}`
         document.querySelector('#temp').innerText = `${Math.floor(data.main.temp)}  C`
         document.querySelector('#humidity').innerText = `${data.main.humidity}  % humidity`
         document.querySelector('#feelsLike').innerText = `Feels like ${Math.floor(data.main.feels_like)} C`
         document.querySelector('#description').innerText = `Description: ${data.weather[0].description}`
-    })
-}
-function ifError(){
-    alert(`Something didn't work well :(\nCheck if you allowed the website to acces your location`);
-}
-const locationButton = document.querySelector('#locationButton') 
-locationButton.addEventListener('click', ()=> {
-    navigator.geolocation.getCurrentPosition(ifSucces, ifError)
-})
-
-
-const search = document.querySelector('#search')
-search.addEventListener("keyup", e =>{
-    if(e.key == "Enter" && search.value != ""){
-        let api = `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${key}`;
-        fetch(api)
-        .then(response => response.json())
-        .then(data => {
-          document.querySelector('#locationName').innerText = `You are currently in ${data.name}, ${data.sys.country}`
-          document.querySelector('#temp').innerText = `${Math.floor(data.main.temp)}  C`
-          document.querySelector('#humidity').innerText = `${data.main.humidity}  % humidity`
-          document.querySelector('#feelsLike').innerText = `Feels like ${Math.floor(data.main.feels_like)} C`
-          document.querySelector('#description').innerText = `Description: ${data.weather[0].description}`
-          search.value = "";
-        })
+        document.querySelector('#infoText').innerText = ''
+        document.querySelector('#weatherDiv').style.display = 'block';
     }
-});
+    search.value = "";
+}
