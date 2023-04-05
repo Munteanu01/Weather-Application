93bb696863f18f200f5792b4aa653cd0c182a31d
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-04-05 20:01:28 +0300

    adding searching cities

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 import { key } from './config.js';
 
-const locationButton = document.querySelector('#locationButton') 
-locationButton.addEventListener('click', ()=> {
-    navigator.geolocation.getCurrentPosition(ifSucces, ifError)
-})
-
+navigator.geolocation.getCurrentPosition(ifSucces, ifError)
 function ifSucces(position){
    const {latitude, longitude} = position.coords;
-   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
    fetch(api)
     .then(response => response.json())
     .then(data => {
-      const city = data.name;
-      alert(`You are currently in ${city}`);
+        document.querySelector('#locationName').innerText = `You are currently in ${data.name}, ${data.sys.country}`
+        document.querySelector('#temp').innerText = `${Math.floor(data.main.temp)}  C`
+        document.querySelector('#humidity').innerText = `${data.main.humidity}  % humidity`
+        document.querySelector('#feelsLike').innerText = `Feels like ${Math.floor(data.main.feels_like)} C`
+        document.querySelector('#description').innerText = `Description: ${data.weather[0].description}`
     })
 }
 function ifError(){
     alert(`Something didn't work well :(\nCheck if you allowed the website to acces your location`);
 }
+const locationButton = document.querySelector('#locationButton') 
+locationButton.addEventListener('click', ()=> {
+    navigator.geolocation.getCurrentPosition(ifSucces, ifError)
+})
+
+
+const search = document.querySelector('#search')
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && search.value != ""){
+        let api = `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${key}`;
+        fetch(api)
+        .then(response => response.json())
+        .then(data => {
+          document.querySelector('#locationName').innerText = `You are currently in ${data.name}, ${data.sys.country}`
+          document.querySelector('#temp').innerText = `${Math.floor(data.main.temp)}  C`
+          document.querySelector('#humidity').innerText = `${data.main.humidity}  % humidity`
+          document.querySelector('#feelsLike').innerText = `Feels like ${Math.floor(data.main.feels_like)} C`
+          document.querySelector('#description').innerText = `Description: ${data.weather[0].description}`
+          search.value = "";
+        })
+    }
+});
