9e76d5b8d8598a62a5de5e88f093ca6ff2541dbc
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-04-07 11:01:16 +0300

    Adding loading

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 import { key } from './config.js';
 
-const search = document.querySelector('#search')
-const locationButton = document.querySelector('#locationButton') 
 let api;
 
+//LOADING
+const loading = document.querySelector('#loading')
+function setLoading() {
+    loading.style.display = 'block';
+    document.querySelector('#weatherDiv').style.display = 'none';
+    document.querySelector('#infoText').innerText = '';
+}
+function clearLoading() {
+    loading.style.display = 'none';
+}
+
+//LOCATION
+const locationButton = document.querySelector('#locationButton') 
 navigator.geolocation.getCurrentPosition(ifSucces, ifError)
 function ifSucces(position){
-   const {latitude, longitude} = position.coords;
-   api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
-   fetchData()
+    setLoading(); 
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
+    fetchData()
 }
 function ifError(){
-    alert(`Something didn't work well :(\nCheck if you allowed the website to acces your location`);
+    alert(`Something didn't work well :(\nCheck if you allowed the website to access your location`);
 }
 locationButton.addEventListener('click', ()=> {
+    setLoading(); 
     navigator.geolocation.getCurrentPosition(ifSucces, ifError)
 })
 
+//SEARCH
+const search = document.querySelector('#search')
 search.addEventListener("keyup", e =>{
     if(e.key == "Enter" && search.value != ""){
         requestApi(search.value)
@@ -24,16 +39,20 @@ search.addEventListener("keyup", e =>{
 })
 function requestApi(city){
     api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+    setLoading(); 
     fetchData();
 }
+
+//RESPONSE
 function fetchData(){
-    fetch(api).then(response => response.json()).then(data => weatherData(data))
+    fetch(api).then(response => response.json()).then(data => weatherData(data)).then(clearLoading());
 }
 function weatherData(data){
     if(data.cod == "404"){
         document.querySelector('#infoText').innerText = `${search.value} isn't a valid city name`;
         document.querySelector('#weatherDiv').style.display = 'none';
     }else{
+        clearLoading();
         document.querySelector('#locationName').innerText = `You are currently in ${data.name}, ${data.sys.country}`
         document.querySelector('#temp').innerText = `${Math.floor(data.main.temp)}  C`
         document.querySelector('#humidity').innerText = `${data.main.humidity}  % humidity`
